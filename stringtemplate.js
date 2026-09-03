85c3749ea71f3e6e133b46be34e9aeb9344aca6b
Yashdeep Yadav <yashdeepyadav997@gmail.com>
2026-09-03 15:21:16 +0530

    Add files via upload

---
+++ stringtemplate.js
@@ -0,0 +1,9 @@
+let first = "Yashdeep"
+let last = "Yadav"
+document.write("My name is"+first+ " " +last);
+console.log('My name is ${first}${last}');
+let fullName = '"My name is"${first}${last}'
+FuntionfullName(first,last)
+{
+return'${first}${last}'
+}
